313803311c405ea3825d8d5d15acf92b903be3d8
GhaythBenAbid <ghaythbenabid0@gmail.com>
2022-07-11 16:58:42 +0100

    add Landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [],
-  darkMode: 'media', // or 'class'
+  darkMode: 'class', // or 'class'
   theme: {
     screens: {
       sm: '640px',
